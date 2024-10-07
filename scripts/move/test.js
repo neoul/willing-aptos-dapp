af30062832b3c68c7d1ad0c6341c0956d83f7427
willing <willing@supervlabs.io>
2024-10-07 18:31:43 +0900

    first commit

---
+++ scripts/move/test.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const cli = require("@aptos-labs/ts-sdk/dist/common/cli/index.js");
+
+async function test() {
+  const move = new cli.Move();
+
+  await move.test({
+    packageDirectoryPath: "contract",
+    namedAddresses: {
+      message_board_addr: "0x100",
+    },
+    extraArguments: [
+      '--move-2'
+    ],
+  });
+}
+test();
